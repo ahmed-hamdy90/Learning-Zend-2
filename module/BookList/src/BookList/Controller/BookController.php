617d8cb34bb1abba71eb22e19aaf15e3b07dbdaa
ahmed-hamdy90 <thespy2030@yahoo.com>
2014-05-02 21:32:38 +0200

    create Book Controller , all it`s views and all routes for it

---
+++ module/BookList/src/BookList/Controller/BookController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BookList\Controller;
+ 
+use Zend\Mvc\Controller\AbstractActionController;
+
+/**
+ * Book Controller
+ *
+ * @author ahmedhamdy <ahmedhmady20@gmail.com>
+ */
+class BookController extends AbstractActionController {
+    
+    /**
+     *  index action to display all books
+     * 
+     */
+    public function indexAction() {
+
+        
+    }
+    
+    /**
+     *  add action to add a new book
+     * 
+     */
+    public function addAction() {
+
+        
+    }
+    
+    /**
+     *  edit action to edit  an existed book
+     * 
+     */
+    public function editAction() {
+
+        
+    }
+    
+    /**
+     *  delete action to delete an existed book
+     * 
+     */
+    public function deleteAction() {
+
+        
+    }
+    
+}
+
+?>
